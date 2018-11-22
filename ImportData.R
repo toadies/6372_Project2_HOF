cca80eedf56640b318393330530b3ced28e2d87b
Chris <chris.ballenger@gmail.com>
2018-11-22 17:44:11 -0600

    Logistic Regression - LASSO

--- ImportData.R
+++ ImportData.R
@@ -3,8 +3,8 @@ train <- read.csv("6372_Project2_HOF/data/train.csv")
 test <- read.csv("6372_Project2_HOF/data/test.csv")
 
 #Columes
-cols.Inducted <- 10
-cols.HallOfFame <- c(2:9)
+cols.Inducted <- 6
+cols.HallOfFame <- c(2:5)
 cols.Batting.avg <- c(100:105)
 cols.Batting <- c(31:48,99)
 cols.Fielding <- c(49:59)
@@ -12,3 +12,5 @@ cols.Awards <- c(50:89)
 cols.Positions <- c(90:98)
 
 names(result)
+
+result[,cols.Inducted]
