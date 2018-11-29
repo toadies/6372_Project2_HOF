64b673059cc268eb9d0551c7fa270e451473eccc
Grant Bourzikas <gbourzikas@smu.edu>
2018-11-28 22:09:05 -0600

    Updated KNN

--- knnModel.R
+++ knnModel.R
@@ -1,4 +1,7 @@
 #################### K Nearest Neighbor #################### 
+library(kknn)
+library(caret)
+library(kableExtra)
 
 #GB SetWD
 source("6372_Project2_HOF/ImportData.R")
@@ -8,7 +11,7 @@ source("6372_Project2_HOF/ImportData.R")
 set.seed(123)
 #knn.train = train(Attrition~., data=emp_train[,c(col.CatKNN)], method="knn", trControl=control, tuneGrid=grid1)
 knn.train = train(HallOfFame_inducted~., data=train[,c(cols.Inducted, cols.KNNData)], method="knn")
-knn.test = knn(train[,c(cols.Inducted, cols.KNNData)][,-1], test[,c(cols.Inducted, cols.KNNData)][,-1], train[,c(cols.Inducted, cols.KNNData)][,1], k=27)
+knn.test = knn(train[,c(cols.Inducted, cols.KNNData)][,-1], test[,c(cols.Inducted, cols.KNNData)][,-1], train[,c(cols.Inducted, cols.KNNData)][,1], k=19)
 knnPrediction <-confusionMatrix(table(knn.test, test$HallOfFame_inducted))
 knnPrediction
 
@@ -22,3 +25,15 @@ kWeightedPrediction
 #graphics.off() 
 #par(mar=c(5,5,5,5))
 plot(kknn.train)
+
+
+# Compare
+dt0 <- data.frame(cbind(t(knnPrediction$overall),t(knnPrediction$byClass)))
+dt0$Type <- as.character("kNN")
+dt1 <- data.frame(cbind(t(kWeightedPrediction$overall),t(kWeightedPrediction$byClass)))
+dt1$Type <- as.character("kWeighted")
+SummaryPred <-rbind(dt0, dt1)
+SummaryPred <- SummaryPred[order(-SummaryPred$Accuracy),]
+SummaryPred <- SummaryPred[,c(19,1:18)]
+SummaryPred  %>%   kable() %>%      kable_styling(bootstrap_options = "striped", full_width = F) %>% scroll_box(width = "700px", height = "200px")
+
